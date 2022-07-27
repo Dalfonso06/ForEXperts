47dbd4bcc488cfbfd3259a0e0c63c1cc141e6020
Dalfonso06 <danielalfons06@gmail.com>
2022-07-27 01:18:26 -0400

    New trade view

--- forEXperts/Controllers/Main Tabs/NewTradeViewController.swift
+++ forEXperts/Controllers/Main Tabs/NewTradeViewController.swift
@@ -9,10 +9,42 @@ import UIKit
 
 class NewTradeViewController: UIViewController {
 
+    @IBOutlet var currency: UIPickerView!
+    
+    @IBOutlet weak var unitAmount: UITextField!
+    
+    let data = ["Bitcoin", "USD", "EUR", "Etherium", "GHB"] // use for the picker view
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        currency.dataSource = self
+        currency.delegate = self
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func sellPressed(_ sender: Any) {
+        print("sold \(unitAmount.text ?? "") units")
+    }
+    
+    @IBAction func buyPressed(_ sender: Any) {
+        print("bought \(unitAmount.text ?? "") units")
+    }
 
+}
+
+extension NewTradeViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return data.count
+    }
+}
+
+extension NewTradeViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return data[row]
+    }
 }
